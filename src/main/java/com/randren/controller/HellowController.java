078064e61225cd326828af39d5befca9e231a44b
YunRanjian <139980214+YunRanjian@users.noreply.github.com>
2024-05-05 02:11:09 +0800

    Commit properties

--- src/main/java/com/randren/controller/HellowController.java
+++ src/main/java/com/randren/controller/HellowController.java
@@ -1,12 +1,31 @@
 package com.randren.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HellowController {
+
+    @Value("${name}")
+    private String name;
+    @Value("${owner.age}")
+    private String age;
+    @Autowired
+    private Environment env;
+    @Autowired
+    private Owner owner;
+
+    /**
+     This is my controller
+     **/
     @RequestMapping("/Hello")
     public String Hellow() {
-        return "Hello World!";
+        return String.format("%s %s %s", owner.getName(), owner.getAge(), env.getProperty("studyDate[0]"));
     }
+
+
+
 }
